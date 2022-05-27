e9fcfca81c3e2d7a217c7fd54e149293c68501fc
EarthmanAman <hashimathman@gmail.com>
2022-05-27 15:15:07 +0300

    redis

--- opl_backend/settings.py
+++ opl_backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CACHEOPS_REDIS = "redis://localhost:6379/1"
+# CACHEOPS_REDIS = "redis://localhost:6379/1"
 
 # Application definition
 
@@ -108,16 +108,16 @@ TEMPLATES = [
 #     },
 # }
 
-# CACHEOPS_REDIS = {
-#     "host": "opl.redis.cache.windows.net",  # redis-server is on same machine
-#     "port": 6379,  # default redis port
-#     "db": 1,  # SELECT non-default redis database
-#     # using separate redis db or redis instance
-#     # is highly recommended
-#     # "socket_timeout": 10,  # connection timeout in seconds, optional
-#     "password": "0HMEsu38GiAYpF7ywUecv9oCqZGRxhWZaAzCaKiAcac=",  # optional
-#     # "unix_socket_path": "opl.redis.cache.windows.net:6380",  # replaces host and port
-# }
+CACHEOPS_REDIS = {
+    "host": "opl.redis.cache.windows.net",  # redis-server is on same machine
+    "port": 6379,  # default redis port
+    "db": 1,  # SELECT non-default redis database
+    # using separate redis db or redis instance
+    # is highly recommended
+    # "socket_timeout": 10,  # connection timeout in seconds, optional
+    "password": "0HMEsu38GiAYpF7ywUecv9oCqZGRxhWZaAzCaKiAcac=",  # optional
+    # "unix_socket_path": "opl.redis.cache.windows.net:6380",  # replaces host and port
+}
 
 # CACHEOPS
 CACHEOPS = {
