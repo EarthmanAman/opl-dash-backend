e48481fff23e51b24a1f21e51c8381499c7e7a00
EarthmanAman <hashimathman@gmail.com>
2022-07-08 13:24:38 +0300

    urls redirect

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ from .views import (
 app_name = "main"
 urlpatterns = [
     path("", FrontendAppView.as_view(), name="frontend"),
+    path("depots", FrontendAppView.as_view(), name="depots_redirect"),
+    path("sale", FrontendAppView.as_view(), name="sales_redirect"),
+    path("customers", FrontendAppView.as_view(), name="customers_redirect"),
 ]
