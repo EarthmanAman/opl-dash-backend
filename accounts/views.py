f5ec678c36e92a15c41b2f313e10db3db8a0b4d0
EarthmanAman <hashimathman@gmail.com>
2022-07-27 12:48:59 +0300

    support multiple depot managers

--- accounts/views.py
+++ accounts/views.py
@@ -3,16 +3,18 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
+
 class CustomAuthToken(ObtainAuthToken):
-    """A view to return a token and information about a user
-    """
+    """A view to return a token and information about a user"""
 
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
 
-        token= Token.objects.get(user=user)
+        token = Token.objects.get(user=user)
         data = {}
         data["id"] = user.id
         data["email"] = user.email
@@ -20,21 +22,33 @@ class CustomAuthToken(ObtainAuthToken):
         data["last_name"] = user.last_name
         message = "Logged in successfully"
 
+        depot_manager = user.depotmanager_set.all()
+
         # GET DEPOT ID
         try:
-            depot_manager = user.depotmanager_set.last()
-            depot_id = depot_manager.depot.id
-            depot_name = depot_manager.depot.name
+            depot = depot_manager.last()
+            depot_id = depot.depot.id
+            depot_name = depot.depot.name
         except:
             depot_id = None
             depot_name = "staff"
 
-        return Response({
-            "status": status.HTTP_200_OK,
-            'message': message,
-            "data":data,
-            "token": token.key,
-            "depot_id":depot_id,
-            "depot": depot_name,
-            "is_staff": user.is_staff
-        })
+        depots = []
+        depots_names = []
+        for depot in depot_manager:
+            depots.append({"name": depot.depot.name, "id": depot.depot.id})
+            depots_names.append(depot.depot.name)
+
+        return Response(
+            {
+                "status": status.HTTP_200_OK,
+                "message": message,
+                "data": data,
+                "token": token.key,
+                "depot_id": depot_id,
+                "depot": depot_name,
+                "depots": depots,
+                "depots_name": depots_names,
+                "is_staff": user.is_staff,
+            }
+        )
